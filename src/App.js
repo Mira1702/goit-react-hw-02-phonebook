a649208c85cdd9198b70f178a078c0dfc158d055
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-02-28 13:21:55 +0200

    выполнен шаг 4

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ class App extends Component {
             {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
             {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
             { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    name: '',
-    number: '',
+    ],    
     filter: ''
   }
 
@@ -50,8 +48,7 @@ class App extends Component {
     });
   };
   
-  render() {
-    const { contacts } = this.state;
+  render() {    
     const { filter } = this.state;
     const filteredContacts = this.getfilteredContacts();
     return (
